355db7e37745372b6b7ae03fcdb42eefa5b79baf
bpn1604 <94555507+Sranjan4321@users.noreply.github.com>
2023-01-22 15:56:24 +0530

    server initial setup

--- sr-chat/server/index.js
+++ sr-chat/server/index.js
@@ -1,4 +1,18 @@
 const http=require("http")
+const exprss=require("express")
+const socketIo=require("socket.io")
 
+const PORT=4001||process.env.PORT
+const app=exprss()
 
-const server=http.createServer()
+app.get("/",(req,res)=>{
+    res.send("server is running")
+})
+const server=http.createServer(app)
+
+const Io=socketIo(server)
+
+server.listen(PORT,()=>{
+    console.log("server is running on PORT",PORT)
+})
+
